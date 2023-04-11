14901227afb86c298221a49373e11c169dc93e56
user.name: David Jaramillo <user.email=jaramillo.david@outlook.com>
2023-04-10 22:04:45 -0500

    initial commit

--- app.js
+++ app.js
@@ -396,6 +396,6 @@ app.get("/room/:roomid/:id/position",function(req,res){
 		}
 	});
 });
-app.listen(3000, function() { 
+app.listen(process.env.PORT || 3000, function() { 
   console.log('Server listening on port 3000'); 
 });
